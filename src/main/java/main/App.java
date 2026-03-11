95f9b1b4fef2ba3a1ea48dc7af85db49809c0791
Antti Romppainen <antti.romppainen@hotmail.com>
2026-03-11 09:52:10 +0100

    Cosmetic fix

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,10 +1,8 @@
 package main;
 
-
-public class App 
-{
-    public static void main( String[] args )
-    {
+// git push origin main
+public class App  {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!!" );
     }
 }
