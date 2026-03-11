8fedfebe17724bd0df23ecd63cdfe9c2dba62def
Antti Romppainen <antti.romppainen@hotmail.com>
2026-03-11 16:33:33 +0100

    Create constructor for RallyRaceResult

--- src/main/java/main/RallyRaceResult.java
+++ src/main/java/main/RallyRaceResult.java
@@ -1,5 +1,15 @@
 package main;
 
+import java.util.Map;
+
 public class RallyRaceResult implements RaceResult {
+    private String raceName;
+    private String location;
+    private Map<Driver, Integer> results;
+
+    public RallyRaceResult (String raceName, String location) {
+        this.raceName = raceName;
+        this.location = location;
+    }
 
 }
