4c9f5635d967cba20528c4da984aa96590553926
Antti Romppainen <antti.romppainen@hotmail.com>
2026-03-11 10:15:30 +0100

    Create AsphaltCar class

--- src/main/java/main/AsphaltCar.java
+++ src/main/java/main/AsphaltCar.java
@@ -1,5 +1,9 @@
 package main;
 
-public class AsphaltCar {
+public class AsphaltCar extends RallyCar {
+
+    public AsphaltCar(String make, String model, int horsePower) {
+        super(make, model, horsePower);
+    }
 
 }
--- src/main/java/main/Rallycar.java
+++ src/main/java/main/Rallycar.java
@@ -2,4 +2,18 @@ package main;
 
 public abstract class RallyCar {
 
+    protected String make;
+    protected String model;
+    protected int horsePower;
+
+    public RallyCar(String make, String model, int horsePower) {
+        this.make = make;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
+
+    abstract double calculatePerformance();
+
+
 }
