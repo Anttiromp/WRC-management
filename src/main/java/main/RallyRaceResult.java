a9edaa1979354b4197c00bb54f29c2d4706e843c
Antti Romppainen <antti.romppainen@hotmail.com>
2026-03-11 16:43:59 +0100

    Override methods for RallyRaceResult & initial ideas for implementing those methods

--- src/main/java/main/RallyRaceResult.java
+++ src/main/java/main/RallyRaceResult.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class RallyRaceResult implements RaceResult {
@@ -17,6 +19,26 @@ public class RallyRaceResult implements RaceResult {
     }
     public String getLocation() {
         return this.location;
+    }
+
+    @Override
+    public void recordResult(Driver driver, int position, int points) {
+        // Tässä metodissa siis, järjestetäänkö kuskit ensin pisteiden mukaan suuruusjärjestykseen, ja sen jälkeen laiteta
+        // LAitetaan ne tuonne results mappiin, hämää koska siinä on vaan Driver ja Integer
+        // VAI
+        // Onko resultissa vain Kuskit ja niitten totalpisteet
+    }
+
+    @Override
+    public int getDriverPoints(Driver driver) {
+        // Ei toimi tavanomainen looppi tässä, katoppas youtubesta oli se video miten pysty semi iisisti noutamaan sen avaimen
+        return 1;
+    }
+
+    @Override
+    public List<Driver> getResults() {
+        // Varmaan vaan joku yksinkertainen tulosta lista tjsp tähän
+        return new ArrayList<>();
     }
 
 }
