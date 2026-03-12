30e2c828c5fce2ababa1694a37ee06ec5fe7e234
Antti Romppainen <antti.romppainen@hotmail.com>
2026-03-12 14:19:43 +0200

    Start writing ChampionshipManager class

--- src/main/java/main/ChampionshipManager.java
+++ src/main/java/main/ChampionshipManager.java
@@ -1,5 +1,12 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChampionshipManager {
+    private List<Driver> drivers = new ArrayList<>();
+    private List<RallyRaceResult> races = new ArrayList<>();
+    private static int totalDrivers;
+    private static int totalRaces;
 
 }
