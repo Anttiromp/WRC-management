b943a1b8b737bff7d0030b153247d6fff29eb24d
Antti Romppainen <antti.romppainen@hotmail.com>
2026-03-12 12:27:46 +0200

    Add stuff for test file

--- src/main/java/main/Test.java
+++ src/main/java/main/Test.java
@@ -5,7 +5,8 @@ public class Test {
         System.out.println("== TEST1: CREATE A NEW DRIVER & CAR == ");
         RallyCar kalleCar = new GravelCar("Toyota", "Coupe", 420);
         Driver kalle = new Driver("Kalle Rovanperä", "Finland", kalleCar);
-        System.out.println(kalle.getCountry() + kalle.getName() + kalle.getTotalPoints());
+        System.out.println("Driver: " + kalle.getName());
+        System.out.println("Car: " + kalle.getCar());
 
     }
 
