e42897a44f2a7b50f9c38f6d7e410008f9aba901
Antti Romppainen <antti.romppainen@hotmail.com>
2026-03-14 15:15:42 +0200

    Create method for average points

--- src/main/java/main/ChampionshipStatistics.java
+++ src/main/java/main/ChampionshipStatistics.java
@@ -1,6 +1,12 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChampionshipStatistics {
+    public static double calculateAveragePointsPerDriver(List<Driver> drivers) {
+        return 0.0;
+    }
 
     
     
