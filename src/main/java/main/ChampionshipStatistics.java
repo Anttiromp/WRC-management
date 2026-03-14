abae2b753dc9f8a8f8a629b1f48302062c9a40a0
Antti Romppainen <antti.romppainen@hotmail.com>
2026-03-14 15:18:16 +0200

    Add method for statistics class

--- src/main/java/main/ChampionshipStatistics.java
+++ src/main/java/main/ChampionshipStatistics.java
@@ -1,12 +1,16 @@
 package main;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class ChampionshipStatistics {
-    public static double calculateAveragePointsPerDriver(List<Driver> drivers) {
+        public static double calculateAveragePointsPerDriver(List<Driver> drivers) {
         return 0.0;
     }
+    public static String findMostSuccessfullCountry(List<Driver> drivers) {
+        return "moi";
+    }
+
 
     
     
